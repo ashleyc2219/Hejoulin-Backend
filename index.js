a518f0033dbdb5e146c583af409868870cb0954a
Wilson#122 <cwhth2956@gmail.com>
2022-03-24 11:52:10 +0800

    製作jwt用middleware

--- index.js
+++ index.js
@@ -18,30 +18,6 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// 註解掉才不會有 token forbidden的問題 不信你試試看
-/* app.use(async (req, res, next) => {
-  if (req.originalUrl === "/login/login") return next();
-  let auth = req.get("Authorization");
-  if (auth && auth.indexOf("Bearer ") === 0) {
-    auth = auth.slice(7);
-    jwt.verify(auth, process.env.JWT_KEY, async (err, member) => {
-      if (err) {
-        res.sendStatus(403);
-      } else {
-        let memberInfo = await db.query(
-          `SELECT a1.user_id, a1.user_account, a2.member_id
-            FROM user AS a1, member AS a2
-             WHERE a1.user_id = a2.user_id AND a1.user_account = ?`,
-          [member.user_account]
-        );
-        res.locals.auth = memberInfo[0][0];
-        next();
-      }
-    });
-  } else {
-    res.sendStatus(403);
-  }
-}); */
 
 // set static folder
 app.use(express.static(__dirname + "/public"));
---
+++ modules/jwtVerify.js
@@ -0,0 +1,27 @@
+const jwt = require("jsonwebtoken");
+const db = require("./modules/connect-db");
+
+function jwtVerify (req, res, next) {
+  if (req.originalUrl === "/login/login") return next();
+  let auth = req.get("Authorization");
+  if (auth && auth.indexOf("Bearer ") === 0) {
+    auth = auth.slice(7);
+    jwt.verify(auth, process.env.JWT_KEY, async (err, member) => {
+      if (err) {
+        res.sendStatus(403);
+      } else {
+        let memberInfo = await db.query(
+          `SELECT a1.user_id, a1.user_account, a2.member_id
+            FROM user AS a1, member AS a2
+             WHERE a1.user_id = a2.user_id AND a1.user_account = ?`,
+          [member.user_account]
+        );
+        res.locals.auth = memberInfo[0][0];
+        next();
+      }
+    });
+  } else {
+    res.sendStatus(403);
+  }
+}
+module.exports = { jwtVerify }
