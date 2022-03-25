f4daeeb9111eeae410024b232f57810461772e9c
Ashley Chen <ashleyc2219@gmail.com>
2022-03-25 13:53:23 +0800

    讀取 訂閱方案/週期

--- routes/sub-time.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-const res = require("express/lib/response");
-var router = express.Router();
-const db = require("../modules/connect-db");
-
-router.get('/subtime', async(req, res)=>{
-    const sql = "SELECT * FROM `sub_plan`";
-    
-})
