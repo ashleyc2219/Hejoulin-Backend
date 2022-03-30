e23cb0015a2d3fad98049e6f2c3058a0c162ddb4
a5757657 <95263890+a5757657@users.noreply.github.com>
2022-03-30 09:43:35 +0800

    改為left join 更改時間格式

--- routes/event.js
+++ routes/event.js
@@ -1,15 +1,21 @@
 const router = require("express").Router();
 const db = require("../modules/connect-db");
+const moment = require("moment-timezone");
 
 // Get all event : http://localhost:3001/api/event
 // Get specific event by event id : http://localhost:3001/api/event?eventId=2
 // Get specific event by event cat  : http://localhost:3001/api/event?eventCatId=1
 router.get("/", async (req, res) => {
+  const fm = ('YYYY-MM-DD HH:MM')
+  const fm2 = ('HH:MM')
   if (req.query.eventId) {
     const { eventId } = req.query;
-    const sql = `SELECT * FROM event WHERE event_id = ${eventId}`;
+    const sql = `SELECT * FROM event e LEFT JOIN event_cat ec ON e.event_cat_id = ec.event_cat_id WHERE event_id = ${eventId}`;
     const [rs, fields] = await db.query(sql);
-    return res.json(rs);
+    const rs2 =rs.map((v)=>({...v, event_time_start: moment(v.event_time_start).format(fm)}))
+    const rs3 =rs2.map((v)=>({...v, event_time_end: moment(v.event_time_end).format(fm2)}))
+    console.log(rs3)
+    return res.json(rs3);
   }
   if (req.query.eventCatId) {
     const { eventCatId } = req.query;
