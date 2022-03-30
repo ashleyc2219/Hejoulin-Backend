b460afc423b9a1c6f4b8cdff0750c16ec6f37ef9
Mary Hsieh <a5925457@gmail.com>
2022-03-30 19:20:35 +0800

    add upload mark

--- routes/mark.js
+++ routes/mark.js
@@ -1,24 +1,23 @@
 const router = require("express").Router();
 const db = require("../modules/connect-db");
+const upload = require("./../modules/upload-images");
 
 // Get mark:http://localhost:3500/api/mark
 // 寫入酒標
-router.post("/", async (req, res) => {
+router.post("/", upload.single("mark"), async (req, res) => {
   const output = {
     success: false,
     error: "",
   };
-
   const sql =
-    "INSERT INTO `mark`(`member_id`,`mark_name`,`pics`,`creat_at`) VALUES (?, ?, ?, NOW())";
+    "INSERT INTO `mark`(`member_id`,`mark_name`,`pics`,`create_at`) VALUES (?, ?, ?, Now())";
   const [result] = await db.query(sql, [
-    req.body.member_id,
-    req.body.mark_name,
-    req.body.pics,
+    4, // 要改登入的member id
+    req.body.markname, // 酒標名稱
+    req.file.filename,
   ]);
   output.success = !!result.affectedRows; //rowcount主為布林職
   output.result = result;
-
   res.json(output);
 });
 
