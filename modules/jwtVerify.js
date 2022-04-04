fd196dbeb703f0a34ab2943999f973071aec4628
Wilson#122 <cwhth2956@gmail.com>
2022-04-04 15:20:29 +0800

    製作分頁

--- modules/jwtVerify.js
+++ modules/jwtVerify.js
@@ -7,22 +7,17 @@ function jwtVerify (req, res, next) {
   let auth = req.get("Authorization");
   if (auth && auth.indexOf("Bearer ") === 0) {
     auth = auth.slice(7);
-    // console.log(auth);
     jwt.verify(auth, process.env.JWT_KEY, async (err, member) => {
       if (err) {
         res.sendStatus(403);
       } else {
-        // console.log(member);
-        // console.log(member.userAccount);
         let memberInfo = await db.query(
           `SELECT a1.user_id, a1.user_account, a2.member_id
             FROM user AS a1, member AS a2
              WHERE a1.user_id = a2.user_id AND a1.user_account = ?`,
           [member.userAccount]
         );
-        // console.log(memberInfo);
         res.locals.auth = memberInfo[0];
-        // console.log(res.locals.auth);
         next();
       }
     });
