f9c37bfdba6652d9a5c3cc91dfc8e202cdd3e2b2
Ashley Chen <ashleyc2219@gmail.com>
2022-04-07 16:48:18 +0800

    讀取訂閱訂單資料

--- routes/sub.js
+++ routes/sub.js
@@ -1,4 +1,5 @@
 var express = require("express");
+const { json } = require("express/lib/response");
 const res = require("express/lib/response");
 var router = express.Router();
 const db = require("../modules/connect-db");
@@ -22,8 +23,7 @@ router.post("/order-sub", async (req, res) => {
 
   const order_id = req.body.order_id;
   const sub_id = req.body.sub_id;
-  // 處理前端傳過來的字串，轉成id數字再存進去 
-  
+  // 處理前端傳過來的字串，轉成id數字再存進去
 
   const sql =
     "INSERT INTO `order_sub_d`(`order_id`, `sub_id`, `subtime_id`, `order_d_price`, `order_state`) VALUES (?,?,?,?,?)";
@@ -105,6 +105,82 @@ router.post("/order-main", async (req, res) => {
   res.json([output, order_id]);
 });
 
+router.get("/order-sub", async (req, res) => {
+  const order_id = req.query.order_id
+    ? parseInt(req.query.order_id)
+    : "20220406004";
+  const sql = `SELECT om.order_id, om.member_id, om.order_name, om.order_mobile, om.order_email, osd.sub_id, osd.subtime_id, osd.order_d_price, st.sub_time_month, st.sub_discount, sd.shipment_method, sd.ship_fee, sd.receiver, sd.receiver_mobile, sd.shipment_address, pd.card_num FROM order_main om 
+            LEFT JOIN order_sub_d osd
+            ON om.order_id = osd.order_id
+            LEFT JOIN sub_time st
+            ON osd.subtime_id = st.subtime_id
+            LEFT JOIN payment_detail pd
+            ON om.order_id=pd.order_id
+            LEFT JOIN shipment_detail sd
+            ON om.order_id=sd.order_id WHERE om.order_id=?;`;
 
+  const [result, fields] = await db.query(sql, [order_id]);
+  const sub_id_arr = JSON.parse(result[0].sub_id);
+  let new_data = [];
+  let info_data = {
+    order_id: result[0]["order_id"],
+    order_name: result[0]["order_name"],
+    order_mobile: result[0]["order_mobile"],
+    order_email: result[0]["order_email"],
+    order_d_price: result[0]["order_d_price"],
+    shipment_method: result[0]["shipment_method"],
+    ship_fee: result[0]["ship_fee"],
+    receiver: result[0]["receiver"],
+    receiver_mobile: result[0]["receiver_mobile"],
+    shipment_address: result[0]["shipment_address"],
+    card_num: result[0]["card_num"],
+  };
+  let rice_percent = function (sub_id) {
+    if (sub_id === 1) {
+      return "70";
+    }
+    if (sub_id === 2) {
+      return "60";
+    }
+    if (sub_id === 3) {
+      return "50";
+    }
+  };
+  let plan_name = function (sub_id) {
+    if (sub_id === 1) {
+      return "純米";
+    }
+    if (sub_id === 2) {
+      return "純米吟釀";
+    }
+    if (sub_id === 3) {
+      return "純米大吟釀";
+    }
+  };
+  let plan_price = function (sub_id) {
+    if (sub_id === 1) {
+      return 1300;
+    }
+    if (sub_id === 2) {
+      return 1500;
+    }
+    if (sub_id === 3) {
+      return 1800;
+    }
+  };
+  sub_id_arr.forEach((sub_id) => {
+    const data_row = {
+      sub_id: sub_id,
+      subtime_id: result[0]["subtime_id"],
+      plan_name: plan_name(sub_id),
+      plan_price: plan_price(sub_id),
+      rice_percent: rice_percent(sub_id),
+      sub_time_month: result[0]["sub_time_month"],
+      sub_discount: result[0]["sub_discount"],
+    };
+    new_data = [...new_data, data_row];
+  });
+  res.json([info_data, new_data]);
+});
 
 module.exports = router;
