3101736c6caf46497bfe9ff93a68c84d65ba6098
Winnie Chien <winnie145ch@gmail.com>
2022-04-08 15:15:14 +0800

    guide

--- routes/product_guide.js
+++ routes/product_guide.js
@@ -9,16 +9,16 @@ router.get("/", async (req, res) => {
     let temp = req.query.pro_temp ? req.query.pro_temp : "";
     let priceLow = req.query.pro_price_low ? req.query.pro_price_low : "";
     let priceHigh = req.query.pro_price_high ? req.query.pro_price_high : "";
-    const gift = req.query.pro_gift ? req.query.pro_gift : "";
+    let gift = req.query.pro_gift ? req.query.pro_gift : "";
 
-    let where = " WHERE 1";
+    let where = ` WHERE 1`;
 
     // 搜尋關鍵字串接
-    if (taste) where += ` AND pf.pro_taste LIKE CONCAT('%',${taste},'%')`;
-    if (temp) where += ` AND pf.pro_temp LIKE CONCAT('%',${temp},'%')`;
+    if (taste) where += ` AND pf.pro_taste LIKE ('%',${taste},'%')`;
+    if (temp) where += ` AND pf.pro_temp LIKE ('%',${temp},'%')`;
     if (priceLow || priceHigh)
       where += ` AND pf.pro_price BETWEEN ${priceLow} AND ${priceHigh}`;
-    if(gift) where += `AND pf.pro_gift>0`
+    if (gift) where += ` AND pf.pro_gift > 0`
 
     const sql = `SELECT * FROM product_sake ps JOIN  product_format pf ON pf.format_id = ps.format_id ${where} ORDER BY rand () LIMIT 3`;
     const [rs, fields] = await db.query(sql);
