2deeea7a63dd2e60f984993a50e0d8c12c169582
Winnie Chien <winnie145ch@gmail.com>
2022-04-10 11:59:25 +0800

    guide correct

--- routes/product_guide.js
+++ routes/product_guide.js
@@ -4,7 +4,7 @@ const db = require("../modules/connect-db");
 // Get kind of gift: http://localhost:3001/api/product_guide
 // 指南推薦酒
 router.get("/", async (req, res) => {
-  if (req.query.ans) {
+  if (req.query) {
     let taste = req.query.pro_taste ? req.query.pro_taste : "";
     let temp = req.query.pro_temp ? req.query.pro_temp : "";
     let priceLow = req.query.pro_price_low ? req.query.pro_price_low : "";
@@ -14,11 +14,11 @@ router.get("/", async (req, res) => {
     let where = ` WHERE 1`;
 
     // 搜尋關鍵字串接
-    if (taste) where += ` AND pf.pro_taste LIKE ('%',${taste},'%')`;
-    if (temp) where += ` AND pf.pro_temp LIKE ('%',${temp},'%')`;
+    if (taste) where += ` AND pf.pro_taste LIKE CONCAT ('%','${taste}','%')`;
+    if (temp) where += ` AND pf.pro_temp LIKE CONCAT ('%','${temp}','%')`;
     if (priceLow || priceHigh)
       where += ` AND pf.pro_price BETWEEN ${priceLow} AND ${priceHigh}`;
-    if (gift) where += ` AND pf.pro_gift > 0`
+    if (!(+gift)) where += ` AND pf.pro_gift > 0`
 
     const sql = `SELECT * FROM product_sake ps JOIN product_format pf ON pf.format_id = ps.format_id ${where} ORDER BY rand () LIMIT 3`;
     const [rs, fields] = await db.query(sql);
